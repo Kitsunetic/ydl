7ff9930bc5433cf3bc9be0b52855eda70f1acec1
Kitsunetic <kitsune.guru@gmail.com>
2019-12-20 01:30:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,32 +2,27 @@ import json
 import os
 import re
 import subprocess
+import time
 
-URLS_TO_DOWNLOAD = [
-    # name, is_video, playlist, path
-    ('cassic2019',  False, 'https://www.youtube.com/playlist?list=PLK4LVBiADYBneFSVyHrFBRFXyK236tGX9', '/mnt/d/Music/2019/classic2019'),
-    ('music2019-3', False, 'https://www.youtube.com/playlist?list=PLK4LVBiADYBnw7IWXiFCdjRYVz5Zz83rz', '/mnt/d/Music/2019/music2019-3'),
-    ('MV2019',      True , 'https://www.youtube.com/playlist?list=PLK4LVBiADYBmnz8MIy9ppxFZytodB9YTT', '/mnt/d/Music/2019/MV2019'),
-    ('Later',       True , 'https://www.youtube.com/playlist?list=PLK4LVBiADYBk_ARjq6DaGvckitGussMRi', '/mnt/e/Video/Youtube_e'),
-]
-
-DATA_FILE = './data.json'
+STORAGE_PATH = '/download'
+DATA_FILE = '/etc/ydl/data.json'
+DOWNLOAD_FILE = '/etc/ydl/download.json'
 
 ydl_path = None
 
 def download_video(youtube_id, path):
     subprocess.Popen(
-        '%s -icw --add-metadata -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]" --ffmpeg-location /usr/local/bin/ffmpeg "https://youtu.be/%s"'%(ydl_path, youtube_id), 
+        'youtube-dl -icw --add-metadata -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]" -o "%(title)s.%(ext)s" https://youtu.be/{}'.format(youtube_id), 
         shell=True, cwd=path, stdout=subprocess.PIPE).stdout.read()
 
 def download_music(youtube_id, path):
     subprocess.Popen(
-        '%s -icw --add-metadata --extract-audio --audio-format mp3 --embed-thumbnail --ffmpeg-location /usr/local/bin/ffmpeg "https://youtu.be/%s"'%(ydl_path, youtube_id), 
+        'youtube-dl -icw --add-metadata --extract-audio --audio-format mp3 --embed-thumbnail -o "%(title)s.%(ext)s" https://youtu.be/{}'.format(youtube_id),
         shell=True, cwd=path, stdout=subprocess.PIPE).stdout.read()
 
 def download_playlist(data, name, is_video, playlist, path):
     res = subprocess.Popen(
-        '%s --flat-playlist --skip-download -j "%s"'%(ydl_path, playlist), 
+        'youtube-dl --flat-playlist --skip-download -j "%s"'%playlist,
         shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8').strip()
     res = '[%s]'%(','.join(res.split('\n')))
     info = json.loads(res)
@@ -55,32 +50,36 @@ def download_playlist(data, name, is_video, playlist, path):
                 print('Download music', youtube_id)
                 download_music(youtube_id, path)
             data['playlist'][name].append(infomap[youtube_id])
-        except:
+        except Exception as e:
             print('[Error] Cannot download', youtube_id)
+            print(e)
+
+
+def refine_file_name(name):
+    return re.sub('[\\*\\\\/"\\?:<>|]+', ' ', name)
+
 
 def main():
-    # get youtube-dl path
-    try:
-        global ydl_path
-        ydl_path = subprocess.Popen(
-            'whereis youtube-dl', shell=True, stdout=subprocess.PIPE
-            ).stdout.read().decode('utf-8').strip()[12:]
-        print('Find youtube-dl path:', ydl_path)
-    except:
-        print('Cannot find youtube-dl installed. Please call `sudo pip install youtube-dl` or `sudo pip3 install youtube-dl`.')
-        return
-    
-    # load data
+    # load data.json
     data = {'playlist': {}}
     if os.path.exists(DATA_FILE):
-        print('Load data from', DATA_FILE)
         with open(DATA_FILE, 'r') as f:
             data = json.load(f)
     
+    # load download.json
+    with open(DOWNLOAD_FILE, 'r') as f:
+        download = json.load(f)
+    
     # download playlist
-    for name, is_video, playlist, path in URLS_TO_DOWNLOAD:
-        print('Start download playlist', name)
-        download_playlist(data, name, is_video, playlist, path)
+    for d in download['download']:
+        print('Start download playlist', d['name'])
+        dpath = os.path.join(STORAGE_PATH, refine_file_name(d['name']))
+        
+        if not os.path.exists(dpath):
+            print('Make directory', dpath)
+            os.makedirs(dpath)
+            
+        download_playlist(data, d['name'], d['is_video'], d['playlist'], dpath)
         
         # save data
         print('Write data into', DATA_FILE)
@@ -88,4 +87,12 @@ def main():
             json.dump(data, f)
 
 if __name__ == "__main__":
-    main()
+    while True:
+        try:
+            main()
+            print('Wait 30min...')
+            print('\n')
+            time.sleep(1800)
+        except KeyboardInterrupt:
+            break
+    print('Program closed...')
